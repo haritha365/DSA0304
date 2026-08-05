49b9f77440d48bc7b34eb57dc3363979ec61f10e
haritha365 <Chereddyharitha955@gmail.com>
2026-08-05 09:24:08 +0530

    Delete EXP5.py

--- EXP5.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem import PorterStemmer
-
-ps = PorterStemmer()
-
-words = ["running", "studies", "playing", "happily"]
-
-print("Stemmed Words:")
-for word in words:
-    print(word, "->", ps.stem(word))
